38e41ec1fec0858d4cf6621a3d120cab9f1761ac
ahmedsgit <acc.ahmedsazzad@gmail.com>
2020-07-06 21:39:34 +0600

    Use spread operator

---
+++ js/spread.js
@@ -0,0 +1,7 @@
+//use spread Operator 
+let oldMembers = ['Sazzad', 'Monir', 'Labib', 'Asif'];
+let newMembers = ['Touhid'];
+
+let members = [...oldMembers, ...newMembers];
+
+console.log(members);
