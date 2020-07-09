08424f27cf7b1070fb0b5c2ca32740478be0cb5a
ahmedsgit <acc.ahmedsazzad@gmail.com>
2020-07-09 08:10:49 +0600

    static keyword

---
+++ ECMAScript-6/static.js
@@ -0,0 +1,11 @@
+/**
+ * Static Keyword - using static, don't need any object to call methods. 
+ */
+
+class hello {
+    static hello() {
+        console.log('Hlw Github Friends.');
+    }
+}
+
+hello.hello();
