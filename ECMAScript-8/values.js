af107af8d9ef8f255ed022e786bed89ff5acb473
ahmedsgit <acc.ahmedsazzad@gmail.com>
2020-07-26 12:24:45 +0600

    Object values and keys

---
+++ ECMAScript-8/values.js
@@ -0,0 +1,10 @@
+/*****
+ * Object.values() & Object.keys()
+ */
+const countries = {
+    BR: 'Brazil',
+    DE: 'Germany',
+    RO: 'Romania',
+    US: 'United States of America'
+};
+console.log(Object.keys(countries), Object.values(countries))
