c249131d1dbfd41e62db3f7e269eea54a504c860
ahmedsgit <acc.ahmedsazzad@gmail.com>
2020-07-26 11:40:49 +0600

    Array includes

---
+++ ECMAScript-7/arrayIncludes.js
@@ -0,0 +1,9 @@
+/*****
+ * Array Includes
+ */
+
+const array = ['Ahmed Sazzad', 'Labib', 25, true, 'friends'];
+
+console.log(array.indexOf('Labib')); //previous way to of finding position
+
+console.log(array.includes('Labib')); //ES7 way
