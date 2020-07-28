06155184dd01e469c1e1a9809006c27744b3474e
ahmedsgit <acc.ahmedsazzad@gmail.com>
2020-07-29 00:39:55 +0600

    CRUD

---
+++ ECMAScript-6/localStorage.js
@@ -0,0 +1,8 @@
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        console.log(xhttp.responseText);
+    }
+};
+xhttp.open("GET", "data.json", true);
+xhttp.send();
