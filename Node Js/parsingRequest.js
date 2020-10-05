a6b571a5e715e3d35e49124bac5d426760b54d14
ahmedsgit <acc.ahmedsazzad@gmail.com>
2020-10-05 12:27:59 +0600

    parsing Request from page

---
+++ Node Js/parsingRequest.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const fs = require('fs');
+
+const Server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title><head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (data) => {
+            console.log(data);
+            body.push(data);
+        });
+        req.on('end', () => {
+            const parseBody = Buffer.concat(body).toString();
+            const message = parseBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
+
+        res.writeHead(302, {
+            'Location': '/'
+        });
+        // res.statusCode = 302;
+        // res.setHeader('Location', '/');
+        return res.end();
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My Node js</title></head>');
+    res.write('<body><h1>Hello from Node js</h1></body>');
+    res.write('</html>');
+    res.end();
+});
+
+Server.listen(3000);
