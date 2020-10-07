de91037bc1b0125ac0ed23e429debe5a88e0b722
ahmedsgit <acc.ahmedsazzad@gmail.com>
2020-10-07 08:00:14 +0600

    parsing Request from page

--- Node Js/parsingRequest.js
+++ Node Js/parsingRequest.js
@@ -17,18 +17,18 @@ const Server = http.createServer((req, res) => {
             console.log(data);
             body.push(data);
         });
-        req.on('end', () => {
+        return req.on('end', () => {
             const parseBody = Buffer.concat(body).toString();
             const message = parseBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-        });
-
-        res.writeHead(302, {
-            'Location': '/'
+            fs.writeFile('message.txt', message, error => {
+                res.writeHead(302, {
+                    'Location': '/'
+                });
+                return res.end();
+            });
         });
         // res.statusCode = 302;
         // res.setHeader('Location', '/');
-        return res.end();
     }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
