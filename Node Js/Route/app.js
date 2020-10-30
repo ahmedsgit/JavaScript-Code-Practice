c0818618c47deff8c9538dc15fa742e92ddc32b5
ahmedsgit <acc.ahmedsazzad@gmail.com>
2020-10-30 18:35:29 +0600

    Middleware Basic

--- Node Js/Route/app.js
+++ Node Js/Route/app.js
@@ -1,23 +1,37 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
-app.get('/about', (req, res) => {
-  res.send('About Page');
+// app.use(morgan('dev'));
+
+function customMiddleWare(req, res, next) {
+  if (req.url === '/help') {
+    res.send('<h1>This Page is Blocked by Admin</h1>');
+  }
+  next();
+}
+
+app.use(customMiddleWare);
+
+app.get('/about', morgan('dev'), (req, res) => {
+  res.send('<h1>About Page</h1>');
+});
+app.get('/help', morgan('dev'), (req, res) => {
+  res.send('<h1>Help Page</h1>');
 });
 
 app.get('/contact', (req, res) => {
-  res.send('Contact Page');
+  res.send('<h1>Contact Page</h1>');
 });
 
 app.get('/', (req, res) => {
-  res.send("I'm Listening");
+  res.send("<h1>I'm Listening</h1>");
 });
 
 app.get('*', (req, res) => {
-  res.send("404 not found");
+  res.send('<h1>404 not found</h1>');
 });
-
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
