174055f5ac3634c5eacd3a8ad2448f032d8cd672
ahmedsgit <acc.ahmedsazzad@gmail.com>
2020-11-22 10:03:46 +0600

    contact model

---
+++ project/Contacts-app/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`SERVER RUNNING ON PORT ${PORT}`);
+});
