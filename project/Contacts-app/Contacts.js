510ae5afb64604106db73055abb5b99b8a0c6187
ahmedsgit <acc.ahmedsazzad@gmail.com>
2020-11-23 19:16:02 +0600

    create and get contatcs

--- project/Contacts-app/Contacts.js
+++ project/Contacts-app/Contacts.js
@@ -1,38 +1,38 @@
 class Contacts {
-  constructor() {
-    this.contacts = [];
-  }
+    constructor() {
+        this.contacts = [];
+    }
 
-  getAllcontacts() {
-    return this.contacts;
-  }
+    getAllContacts() {
+        return this.contacts;
+    }
 
-  getcontactsById(id) {
-    return this.contacts.find(contact => contact.id === id);
-  }
+    getContactsById(id) {
+        return this.contacts.find(contact => contact.id === id);
+    }
 
-  createContact(contact) {
-    contact.id = this.contacts.length + 1;
-    this.contacts.push(contact);
-    return contact;
-  }
+    createContact(contact) {
+        contact.id = this.contacts.length + 1;
+        this.contacts.push(contact);
+        return contact;
+    }
 
-  updateContactById(id, updatedContact) {
-    let index = this.contacts.findIndex(contact => contact.id === id);
+    updateContactById(id, updatedContact) {
+        let index = this.contacts.findIndex(contact => contact.id === id);
 
-    this.contacts[index].name = updatedContact.name || this.contacts[index].name;
-    this.contacts[index].email = updatedContact.email || this.contacts[index].email;
-    this.contacts[index].phone = updatedContact.phone || this.contacts[index].phone;
+        this.contacts[index].name = updatedContact.name || this.contacts[index].name;
+        this.contacts[index].email = updatedContact.email || this.contacts[index].email;
+        this.contacts[index].phone = updatedContact.phone || this.contacts[index].phone;
 
-    return this.contacts[index];
-  }
+        return this.contacts[index];
+    }
 
-  deleteContactById(id){
-    let index = this.contacts.findIndex(contact => contact.id === id);
-    let deletedObj = this.contacts[index];
-    this.contacts = this.contacts.filter(contact => contact.id !== id);
-    return deletedObj;
-  }
+    deleteContactById(id) {
+        let index = this.contacts.findIndex(contact => contact.id === id);
+        let deletedObj = this.contacts[index];
+        this.contacts = this.contacts.filter(contact => contact.id !== id);
+        return deletedObj;
+    }
 }
 
-exports.contacts = new Contacts()
+module.exports = new Contacts()
